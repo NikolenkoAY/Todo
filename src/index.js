4912fe32e9a00765a71d635306f1a244e0e6afe5
Alexey Nikolenko <nikolenkoay@gmail.com>
2019-03-20 22:10:07 +0300

    JSX lesson

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const TodoList = () => {
+  const items = ["Learn React", "Create App"];
   return (
     <ul>
-      <li>Create App</li>
-      <li>Create cool App</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
   );
 };
 
 const AppHeader = () => <h1>My Todo list</h1>;
 
-const SearchPanel = () => <input placeholder="search" />;
+const SearchPanel = () => {
+  const searchText = "Type to search";
+  const searchStyle = {
+    fontSize: "15px"
+  };
+
+  return <input style={searchStyle} placeholder={searchText} />;
+};
 
 const App = () => {
   return (
