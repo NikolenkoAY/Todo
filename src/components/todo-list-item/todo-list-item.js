c5a5484e750d17fdb1492e1010f8f4cca924864b
Alexey Nikolenko <nikolenkoay@gmail.com>
2019-03-25 21:42:11 +0300

    setState() - lesson 37

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,50 +4,49 @@ import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
   state = {
-    done: false
+    done: false,
+    important: false
   };
   onLabelClick = () => {
     this.setState({
       done: true
     });
   };
-
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = "todo-list-item ";
     if (done) {
       classNames += " done";
     }
 
-    const style = {
-      color: important ? "tomato" : "black",
-      fontWeight: important ? "bold" : "normal"
-    };
+    if (important) {
+      classNames += " important";
+    }
 
     return (
       <span className={classNames}>
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
-
-        <button
-          type="button"
-          className="btn btn-outline-success btn-sm float-right"
-        >
-          <i className="fa fa-exclamation" />
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-danger btn-sm float-right"
-        >
-          <i className="fa fa-trash-o" />
-        </button>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-outline-success btn-sm "
+            onClick={this.onMarkImportant}
+          >
+            <i className="fa fa-exclamation" />
+          </button>
+          <button className="btn btn-outline-danger btn-sm ">
+            <i className="fa fa-trash-o" />
+          </button>
+        </div>
       </span>
     );
   }
